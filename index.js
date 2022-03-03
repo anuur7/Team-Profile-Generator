0845a34de6d285f18d50fccb482416de47923a93
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-03 15:36:33 +0000

    added 2 more engineer questions

--- index.js
+++ index.js
@@ -1,23 +1,92 @@
 const inquirer = require('inquirer');
 const { promises: fs } = require('fs')
 
-async function managerQuestions() {
+async function Questions() {
     const answers = await inquirer.prompt([{
+        // MANAGER QUESTIONS
         type: 'input',
-        name: 'name',
-        message: 'What is your name manager?'
+        name: 'manager name',
+        message: 'Hi team manager, what is your name?'
     }, {
         type: 'input',
-        name: 'id',
+        name: 'manager id',
         message: 'Enter your ID number'
     }, {
         type: 'input',
-        name: 'email',
-        message: 'Enter your email address?'
+        name: 'manager email',
+        message: 'Enter your email address'
     }, {
         type: 'input',
-        name: 'office number',
+        name: 'manager office number',
         message: 'What is your office number?'
+    }, {
+        // ENGINEER QUESTIONS
+        type: 'input',
+        name: 'engineer name',
+        message: 'Hi engineer, what is your name?'
+    }, {
+        type: 'input',
+        name: 'engineer id',
+        message: 'Enter your ID number'
+    }, {
+        type: 'input',
+        name: 'engineer email',
+        message: 'Enter your email address'
+    }, {
+        type: 'input',
+        name: 'engineer github',
+        message: 'What is your github username?'
+    }, {
+        // ENGINEER 2 QUESTIONS
+        type: 'input',
+        name: 'engineer 2 name',
+        message: 'Hi engineer 2, what is your name?'
+    }, {
+        type: 'input',
+        name: 'engineer 2 id',
+        message: 'Enter your ID number'
+    }, {
+        type: 'input',
+        name: 'engineer 2 email',
+        message: 'Enter your email address'
+    }, {
+        type: 'input',
+        name: 'engineer 2 github',
+        message: 'What is your github username?'
+    }, {
+        //ENGINEER 3 QUESTIONS
+        type: 'input',
+        name: 'engineer 3 name',
+        message: 'Hi engineer 3, what is your name?'
+    }, {
+        type: 'input',
+        name: 'engineer 3 id',
+        message: 'Enter your ID number'
+    }, {
+        type: 'input',
+        name: 'engineer 3 email',
+        message: 'Enter your email address'
+    }, {
+        type: 'input',
+        name: 'engineer 3 github',
+        message: 'What is your github username?'
+    }, {
+        //INTERN QUESTIONS
+        type: 'input',
+        name: 'intern name',
+        message: 'Hi intern, what is your name?'
+    }, {
+        type: 'input',
+        name: 'intern id',
+        message: 'Enter your ID number'
+    }, {
+        type: 'input',
+        name: 'intern email',
+        message: 'Enter your email address'
+    }, {
+        type: 'input',
+        name: 'intern school',
+        message: 'What school did you study at?'
     }])
 
     const templateContent = await fs.readFile('./index.html');
@@ -28,4 +97,4 @@ async function managerQuestions() {
     await fs.writeFile('index.html', newTemplateContent)
 }
 
-managerQuestions()
+Questions()
