b6146663cbc14e76288d1bf70501b55e35b48ae8
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-03-03 13:46:07 +0000

    inquirer working

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const inquirer = require('inquirer');
 
 async function managerQuestions() {
-    console.log('WORKING')
+    const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name manager?'
+    }, {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your ID number'
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address?'
+    }, {
+        type: 'input',
+        name: 'office number',
+        message: 'What is your office number?'
+    }])
+    
+    console.log(answers)
 }
 
 managerQuestions()
